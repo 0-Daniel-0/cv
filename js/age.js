4b857704e57ff4eb3620279202e9bb41396a17da
Daniel <73481380+0-Daniel-0@users.noreply.github.com>
2025-10-29 17:49:53 +0100

    Update age.js
    Age update

--- js/age.js
+++ js/age.js
@@ -1,9 +1,9 @@
-var birthday = new Date("2003");
+var birthday = new Date('2002')
 
 function updateaAge() {
-    var ageElement = document.getElementById("age");
-    var age = new Date().getFullYear() - birthday.getFullYear();
-    ageElement.textContent = age + " jaar";
+	var ageElement = document.getElementById('age')
+	var age = new Date().getFullYear() - birthday.getFullYear()
+	ageElement.textContent = age + ' jaar'
 }
 
-updateaAge();
+updateaAge()
